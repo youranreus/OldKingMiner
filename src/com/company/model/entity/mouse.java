6255ef35f9f214e9f5ab188d88f79ba8d9d5332d
youranreus <youranreus@qq.com>
2021-10-15 20:58:09 +0800

    fix: wrong parameter of constructor in mouse.java

--- src/com/company/model/entity/mouse.java
+++ src/com/company/model/entity/mouse.java
@@ -7,8 +7,8 @@ public class mouse extends item{
      */
     private final int price;
 
-    public mouse(String _name, int id) {
-        super(_name, 0, 0, id);
+    public mouse(String _name, int _id) {
+        super(_name, 0, 0, _id);
         this.price = (int)(1+Math.random()*(10-2+1));
     }
 
