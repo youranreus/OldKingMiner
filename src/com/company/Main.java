b58bdd1fc3758e42aca18c21045bd1473d891dc4
youranreus <youranreus@qq.com>
2021-10-16 10:13:48 +0800

    test: basic game running

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import com.company.controller.GameProcessController;
+import com.company.controller.GameController;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // write your code here
+        GameController game = new GameController(2);
+        GameProcessController c = new GameProcessController(game);
+        c.start();
     }
 }
