0dc00beec7feee2af43a2f71cd2dbcab2e430e6e
youranreus <youranreus@qq.com>
2021-10-22 11:09:20 +0800

    update: add rock and diamond

---
+++ src/com/company/model/entity/diamond.java
@@ -0,0 +1,23 @@
+package com.company.model.entity;
+
+public class diamond extends mineral{
+
+    /**
+     * diamond constructor
+     *
+     * @param _name 物品名
+     * @param _id id
+     * @param _size 物体大小
+     */
+    diamond(String _name, int _id, int _size) {
+        super(_name, _id, _size);
+        this.size = _size;
+        if (size == 1)
+            this.price = 1000;
+        else if (size == 2)
+            this.price = 2000;
+        else
+            this.price = 6000;
+    }
+
+}
---
+++ src/com/company/model/entity/rock.java
@@ -0,0 +1,22 @@
+package com.company.model.entity;
+
+public class rock extends mineral{
+
+    /**
+     * rock constructor
+     *
+     * @param _name 物品名
+     * @param _id   id
+     * @param _size 物体大小
+     */
+    rock(String _name, int _id, int _size) {
+        super(_name, _id, _size);
+        this.size = _size;
+        if (size == 1)
+            this.price = 10;
+        else if (size == 2)
+            this.price = 30;
+        else
+            this.price = 60;
+    }
+}
