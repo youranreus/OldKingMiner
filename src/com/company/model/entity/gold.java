acc344187079d5347fe5b4a28257c54b77e6d02f
youranreus <youranreus@qq.com>
2021-10-22 11:09:39 +0800

    fix: gold

--- src/com/company/model/entity/gold.java
+++ src/com/company/model/entity/gold.java
@@ -1,20 +1,6 @@
 package com.company.model.entity;
 
-public class gold extends item {
-
-    /**
-     * 金矿大小
-     * 大：3
-     * 中：2
-     * 小：1
-     */
-    private final int size;
-
-    /**
-     * 金矿价值
-     * 100-300-600
-     */
-    private final int price;
+public class gold extends mineral {
 
     /**
      * gold constructor
@@ -24,7 +10,7 @@ public class gold extends item {
      * @param _size 物体大小
      */
     gold(String _name, int _id, int _size) {
-        super(_name, 0, 0, _id);
+        super(_name, _id, _size);
         this.size = _size;
         if (size == 1)
             this.price = 100;
@@ -33,23 +19,5 @@ public class gold extends item {
         else
             this.price = 600;
     }
-
-    /**
-     * 获取金矿大小
-     * @return int size
-     */
-    int getSize() {
-        return size;
-    }
-
-    /**
-     * 获取金矿价值
-     * @return int price
-     */
-    int getPrice() {
-        return price;
-    }
-
-
 
 }
