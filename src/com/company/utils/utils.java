4983d2931a64cdcd5828329992b4fa6e07ffaf0a
youranreus <youranreus@qq.com>
2021-10-22 12:48:42 +0800

    update: random function in utils.java

--- src/com/company/utils/utils.java
+++ src/com/company/utils/utils.java
@@ -2,4 +2,15 @@ package com.company.utils;
 
 public class utils {
 
+    /**
+     * 随机数
+     *
+     * @param min 最小值
+     * @param max 最大值
+     * @return int 随机数
+     */
+    public static int random(int min, int max) {
+        return (int) (Math.random()*(max-min)+min);
+    }
+
 }
