c27da7bf3745ac6e3a4501874c01c30dd4281435
youranreus <youranreus@qq.com>
2021-10-22 17:02:42 +0800

    update: add background

--- src/com/company/controller/MenuController.java
+++ src/com/company/controller/MenuController.java
@@ -3,18 +3,31 @@ package com.company.controller;
 import com.company.utils.config;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MenuController extends JFrame {
+
+    Image background = Toolkit.getDefaultToolkit().getImage("assets/img/bg.png");
 
     /**
      * 界面初始化
      */
     void launch() {
         this.setVisible(true);
-        this.setSize(700, 500);
+        this.setSize(800, 400);
         this.setLocationRelativeTo(null);
         this.setTitle("老金矿工@" + config.VERSION);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    /**
+     * 绘制
+     *
+     * @param g 图形
+     */
+    @Override
+    public void paint(Graphics g) {
+        g.drawImage(this.background, 0,0,null);
     }
 
     /**
