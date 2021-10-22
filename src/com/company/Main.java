74121141c5f7e8eface1b9c7728387573c12b314
youranreus <youranreus@qq.com>
2021-10-22 15:46:50 +0800

    feat: basic Frame display

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,13 +2,14 @@ package com.company;
 
 import com.company.controller.GameProcessController;
 import com.company.controller.GameController;
+import com.company.controller.MenuController;
 
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
-        GameController game = new GameController(2);
-        GameProcessController c = new GameProcessController(game);
-        c.start();
+        MenuController gui = new MenuController();
+//        GameController game = new GameController(2);
+//        GameProcessController c = new GameProcessController(game);
+//        c.start();
     }
 }
--- src/com/company/controller/MenuController.java
+++ src/com/company/controller/MenuController.java
@@ -1,7 +1,27 @@
 package com.company.controller;
 
-public class MenuController extends BaseController{
+import com.company.utils.config;
 
+import javax.swing.*;
 
+public class MenuController extends JFrame {
+
+    /**
+     * 界面初始化
+     */
+    void launch() {
+        this.setVisible(true);
+        this.setSize(700, 500);
+        this.setLocationRelativeTo(null);
+        this.setTitle("老金矿工@" + config.VERSION);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    /**
+     * MenuController constructor.
+     */
+    public MenuController() {
+        this.launch();
+    }
 
 }
