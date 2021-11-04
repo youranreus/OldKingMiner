d9109938f468ffe7abd921e56467f8f420171328
youranreus <youranreus@qq.com>
2021-11-04 20:45:21 +0800

    update: 0.0.5

--- src/com/company/utils/config.java
+++ src/com/company/utils/config.java
@@ -2,7 +2,7 @@ package com.company.utils;
 
 public class config {
 
-    public static final String VERSION = "0.0.4";
+    public static final String VERSION = "0.0.5";
 
     public static final boolean DEBUG = true;
 
