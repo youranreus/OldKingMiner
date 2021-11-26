2f998731820d0af12988946f407b7ad31927d15b
youranreus <youranreus@qq.com>
2021-11-26 16:09:02 +0800

    update: 添加联机选项

--- src/com/company/utils/config.java
+++ src/com/company/utils/config.java
@@ -2,8 +2,32 @@ package com.company.utils;
 
 public class config {
 
+    /**
+     * 版本
+     */
     public static final String VERSION = "0.0.5";
 
+    /**
+     * 调试模式
+     */
     public static final boolean DEBUG = true;
+
+    /**
+     * 在线模式
+     * 0 => 本地模式
+     * 1 => 主机模式
+     * 2 => 联机模式
+     */
+    public static final int online = 0;
+
+    /**
+     * 需要连接到的服务器地址
+     */
+    public static final String server = "";
+
+    /**
+     * 需要连接到的服务器端口
+     */
+    public static final String port = "";
 
 }
