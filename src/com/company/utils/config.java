5f76f4e566f26e551f4aca4e43ce3800ca81ec64
youranreus <youranreus@qq.com>
2021-12-25 10:09:56 +0800

    update: version 1.0.0

--- src/com/company/utils/config.java
+++ src/com/company/utils/config.java
@@ -5,7 +5,7 @@ public class config {
     /**
      * 版本
      */
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "1.0.0";
 
     /**
      * 调试模式
@@ -23,7 +23,7 @@ public class config {
     /**
      * 需要连接到的服务器地址
      */
-    public static final String server = "10.161.198.233";
+    public static final String server = "127.0.0.1";
 
     /**
      * 需要连接到的服务器端口
